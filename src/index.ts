4b1fc27758df458670c639918e9fb36a9be27dbe
김지호 <jiho001108@gmail.com>
2024-05-13 19:48:58 +0900

    Add Block

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-console.log("HI");
-console.log("bye");
+import crypto from "node:crypto";
+
+interface BlockShape {
+	hash: string;
+	prevHash: string;
+	height: number;
+	data: string;
+}
+
+class Block implements BlockShape {
+	public hash: string;
+	constructor(
+		public prevHash: string,
+		public height: number,
+		public data: string,
+	) {
+		this.hash = Block.calculateHash(prevHash, height, data);
+	}
+	static calculateHash(prevHash: string, height: number, data: string) {
+		const toHash = `${prevHash}${height}${data}`;
+	}
+}
